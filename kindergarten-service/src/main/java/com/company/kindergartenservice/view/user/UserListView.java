cf53dec6dd41f5772424099d814d38a508d6dd89
i.kineev <i.kineev@argustelecom.ru>
2025-09-16 13:17:59 +0300

    init project

---
+++ kindergarten-service/src/main/java/com/company/kindergartenservice/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.kindergartenservice.view.user;
+
+import com.company.kindergartenservice.entity.User;
+import com.company.kindergartenservice.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "kind_User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
---
+++ staff-service/src/main/java/com/company/staffservice/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.staffservice.view.user;
+
+import com.company.staffservice.entity.User;
+import com.company.staffservice.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "staff_User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
