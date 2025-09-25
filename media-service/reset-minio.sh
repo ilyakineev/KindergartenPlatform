06092036762225827fbd2663716b8517b985d4eb
i.kineev <i.kineev@argustelecom.ru>
2025-09-25 16:22:08 +0300

    add reset sh

---
+++ media-service/reset-minio.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+CONTAINER_NAME="kindergarten-minio"
+VOLUME_NAME="docker_kindergarten_minio_data"
+
+echo "Остановка и удаление контейнера: $CONTAINER_NAME"
+docker rm -f $CONTAINER_NAME || true
+
+echo "Удаление тома: $VOLUME_NAME"
+docker volume rm $VOLUME_NAME || true
+
+echo "Запуск нового контейнера через docker-compose"
+docker-compose -f docker/docker-compose.yml up -d minio
